5e53c5214c091a009e356f210e460770226cf9df
indongyoo <idy@marpple.com>
2024-03-12 22:53:05 +0900

    feat: add event decorator, refactoring

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,5 @@ export { Enable, ListEnable } from './Enable';
 export { Layout } from './Layout';
 export { LayoutHelper } from './LayoutHelper';
 export { Page } from './Page';
-export {
-  UnsafeHtml,
-  VirtualView,
-  type TemplateReturnType,
-  type Html,
-  html,
-} from './VirtualView';
+export { UnsafeHtml, VirtualView, type Html, html } from './VirtualView';
 export { on } from './decorators';
