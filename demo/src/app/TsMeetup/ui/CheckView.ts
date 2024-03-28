a8d1bcd345b12a8ace624aa7342070c59f969bf0
indongyoo <idy@marpple.com>
2024-03-28 13:27:08 +0900

    refactor: remove demo code

--- demo/src/app/TsMeetup/ui/CheckView.ts
+++
@@ -1,8 +0,0 @@
-import { ToggleView } from './ToggleView';
-import { html } from 'rune-ts';
-
-export class CheckView extends ToggleView {
-  override template() {
-    return html`<span class="${this.data.on ? 'on' : ''}"></span>`;
-  }
-}
