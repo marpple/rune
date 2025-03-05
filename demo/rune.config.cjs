d0d036752dd9f79507974e58b37be4a903f4e8ef
jeongjaehun <jhj@marpple.com>
2025-03-05 16:47:46 +0900

    chore: demo setting

--- demo/rune.config.cjs
+++ demo/rune.config.cjs
@@ -3,13 +3,14 @@ const { join, resolve } = require('node:path');
  * @type {import('@rune-ts/server').RuneConfigType}
  */
 module.exports = {
-  port: 5001,
+  port: 5002,
   hostname: 'localhost',
   mode: 'render',
   sassOptions: {
     api: 'legacy',
     includePaths: [join(resolve(), 'common/style')],
     additionalData: `@import "base";`,
+    silenceDeprecations: ['import'],
   },
   clientEntry: './src/app/client.ts',
   serverEntry: './src/server/server.ts',
