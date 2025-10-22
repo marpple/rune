49f332d38f1204fdd4bd9d5cf62edce0ab229240
Sunhyoup <kciter@naver.com>
2025-10-22 19:24:28 +0900

    fix: Remove log

--- examples/todo-setting/src/app/lib/CheckView.ts
+++ examples/todo-setting/src/app/lib/CheckView.ts
@@ -5,12 +5,4 @@ export class CheckView extends ToggleView {
   override template() {
     return html` <span class="${this.data.on ? 'on' : ''}"></span> `;
   }
-
-  override onMount(): void {
-    console.log('CheckView mounted');
-  }
-
-  override onUnmount(): void {
-    console.log('CheckView unmounted');
-  }
 }
