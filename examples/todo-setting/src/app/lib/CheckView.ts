02f8f3fa7f29e11908bc6d1cb86cee535bb8a8ed
Sunhyoup <kciter@naver.com>
2025-10-22 18:23:30 +0900

    fix: Ensure onMount is called during SSR hydration

--- examples/todo-setting/src/app/lib/CheckView.ts
+++ examples/todo-setting/src/app/lib/CheckView.ts
@@ -5,4 +5,12 @@ export class CheckView extends ToggleView {
   override template() {
     return html` <span class="${this.data.on ? 'on' : ''}"></span> `;
   }
+
+  override onMount(): void {
+    console.log('CheckView mounted');
+  }
+
+  override onUnmount(): void {
+    console.log('CheckView unmounted');
+  }
 }
